a3393d9940a119792d8a4c95a9906e772e3f27e1
fjronero99 <fjronero99@gmail.com>
2025-10-21 11:08:52 -0500

    Setp pages

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,7 @@
+export default function Upload() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-3xl font-semibold">Upload Page 📤</h1>
+    </div>
+  );
+}
---
+++ frontend/src/pages/Upload.jsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-3xl font-semibold">Login Page 🔐</h1>
+    </div>
+  );
+}
